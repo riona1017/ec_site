53d66a161bc188f88962be75d39c5c16f9840531
riona1017 <riona10171933@gmail.com>
2023-02-08 20:26:45 +0900

    toastr

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,12 +1,12 @@
 @props(['status' => 'info'])
 
-@php
-    if($status === 'info'){$bgColor = 'bg-blue-300';}
-    if($status === 'error'){$bgColor = 'bg-red-500';}
-@endphp
-
-@if(session('message'))
-    <div class="{{ $bgColor }} w-1/3 mx-auto p-5 text-white">
-        {{ session('message') }}
-    </div>
-@endif
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script>
+    @if(session('message'))
+        $(function () {
+            toastr.success("{{ session('message') }}");
+        });
+    @endif
+</script>
