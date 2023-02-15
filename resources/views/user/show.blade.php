42d2352b929045530b1cfce9e26d0dffa900cae1
riona1017 <riona10171933@gmail.com>
2023-02-15 10:14:42 +0900

    swiper

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -16,10 +16,35 @@
                                 <!-- Additional required wrapper -->
                                 <div class="swiper-wrapper">
                                 <!-- Slides -->
-                                <div class="swiper-slide"><img src="{{ asset('storage/products/sample1.jpg') }}"></div>
-                                <div class="swiper-slide"></div>
-                                <div class="swiper-slide">Slide 3</div>
-                                ...
+                                <div class="swiper-slide">
+                                    @if ($product->imageFirst->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageFirst->filename) }}">
+                                    @else
+                                        <img src="">
+                                    @endif
+                                </div>
+                                <div class="swiper-slide">
+                                    @if ($product->imageSecond->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageSecond->filename) }}">
+                                    @else
+                                        <img src="">
+                                    @endif
+                                </div>
+                                <div class="swiper-slide">
+                                    @if ($product->imageThird->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageThird->filename) }}">
+                                    @else
+                                        <img src="">
+                                    @endif
+                                </div>
+                                <div class="swiper-slide">
+                                    @if ($product->imageFourth->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageFourth->filename) }}">
+                                    @else
+                                        <img src="">
+                                    @endif
+                                </div>
+                                
                                 </div>
                                 <!-- If we need pagination -->
                                 <div class="swiper-pagination"></div>
@@ -59,5 +84,4 @@
             </div>
         </div>
     </div>
-
 </x-app-layout>
