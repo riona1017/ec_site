72f4305419eeaf840102f73fef0cdb0ad92cf5ed
riona1017 <riona10171933@gmail.com>
2023-02-17 14:28:48 +0900

    ユーザー向け商品購入メール

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Stock;
 use App\Services\CartService;
+use App\Jobs\SendThanksMail;
 
 class CartController extends Controller
 {
@@ -60,6 +61,10 @@ class CartController extends Controller
         ////
         $items = Cart::where('user_id', Auth::id())->get();
         $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        dd('ユーザーメール送信テスト');
         ////
 
 
--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -33,9 +33,6 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        //非同期に送信
-        SendThanksMail::dispatch();
-
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
